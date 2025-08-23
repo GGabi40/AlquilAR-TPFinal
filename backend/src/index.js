a10d99e44b7602c080fec5ec82171c0e03c139ad
ggabi40 <gabibc2000@hotmail.com>
2025-08-23 18:41:48 -0300

    Add initial backend setup with Express, CORS, and Sequelize configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,25 @@
-import e from 'express';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import { port, sequelize } from './config/db.js';
+
+dotenv.config();
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.use(express.json());
+
+try {
+    await sequelize.sync();
+    app.listen(port, () => {
+        console.log(`Corriendo servidor en http://localhost:${port}`);
+    });
+} catch(e) {
+    console.error('Error al inicializar servidor: ', error);
+}
